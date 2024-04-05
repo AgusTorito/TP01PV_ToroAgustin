7289063bac8408e06a8ad287d5156042ff21f572
Agus <Agus@DESKTOP-MNBU4VL>
2024-04-05 09:03:54 -0300

    ejercicio02_TP01PV

--- src/ar/edu/unju/fi/ejercicio02/Main.java
+++ src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -1,23 +1,26 @@
 package ar.edu.unju.fi.ejercicio02;
 
 public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
-	public static void main(String[] args)
-	{
-		//TODO Auto-generated method stub
-		String NomPais = "Argentina";
-		int PersonaE = 20;
-		float AltEd = 20.5f;
-		float ProdSuper = 200.99f;
-		double Telefono = 3884773599d;
-		double CalCoseno = Math.cos(0.5);
-		
-		System.out.println("El nombre del pais es: "+NomPais);
-		System.out.println("La edad de una persona: "+PersonaE);
-		System.out.println("La altura de un edificio: "+AltEd);
-		System.out.println("El precio de un producto del Supermercado: "+ProdSuper);
-		System.out.println("Numero de telefono de esa persona: "+Telefono);
-		System.out.println("El calculo de coseno de 0.5: "+CalCoseno);
-	}
-	
+				//TODO Auto-generated method stub
+				String NomPais = "Argentina";
+				int PersonaE = 20;
+				float AltEd = 20.5f;
+				float ProdSuper = 200.99f;
+				double Telefono = 3884773599d;
+				double CalCoseno = Math.cos(0.5);
+				
+				System.out.println("El nombre del pais es: "+NomPais);
+				System.out.println("La edad de una persona: "+PersonaE);
+				System.out.println("La altura de un edificio: "+AltEd);
+				System.out.println("El precio de un producto del Supermercado: "+ProdSuper);
+				System.out.println("Numero de telefono de esa persona: "+Telefono);
+				System.out.println("El calculo de coseno de 0.5: "+CalCoseno);
+			
+			
+		}
+
 }
