eff4a90d005aa2579a174322534db1ccd56d2b78
Agus <Agus@DESKTOP-MNBU4VL>
2024-04-13 09:46:14 -0300

    Ejercicio 11

---
+++ src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		int suma = 0;
+		
+		for( i = 2, j = 40; j > 0; j--)
+		{
+			suma = j*i;
+			i++;
+			System.out.printf("%d ", + suma);
+		}
+        
+	}
+
+}
